735204a153e4c9161f5dc1017cddacff76a9166c
tom <tomadav@hotmail.com>
2017-07-21 10:28:13 +0200

    creation index
    3 pages HTML

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+//page d'accueil de biovillefranche
+$titrePage = 'BioVillefranche - Votre épicerie bio';
+
+include 'include/header.php';
+ ?>
+ <h1>Bienvenue dans votre épicerie Bio de Villefranche</h1>
+ <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+</p>
+
+<?php
+//pour vérifier la connexion*
+//print_r($dbh);
+//pied de page
+include 'include/footer.php'
+ ?>
